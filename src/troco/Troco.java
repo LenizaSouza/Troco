29c6090e1593a615550862ff9de2808f7cdca458
Leniza Souza <lenizarb@gmail.com>
2023-02-10 20:27:42 -0300

    troco em moedas

--- src/troco/Troco.java
+++ src/troco/Troco.java
@@ -6,49 +6,37 @@ import java.util.Scanner;
 public class Troco {
 
 	public static void main(String[] args) {
-		Scanner ler = new Scanner(System.in);		
-	
+		Scanner ler = new Scanner(System.in);
 		double conta;
-		double pago;	
-		   
-			
-		}
-	public static String calculaTroco(double conta, double pago) {
-		DecimalFormat formatador = new DecimalFormat ("###,##0.00");
+		double pago;
 		double troco;
-		double  vlr, ct;
-		int i;
-		String result;
-		double centavos[] = {0.25, 0.10, 0.5, 0.1};	
-		
-		conta = 1.0;
-		pago =  0.88;
+		String result="";;
+		conta = 0.88;
+		pago = 1.00;
 		troco = pago - conta;
-		System.out.println(troco);
-		 
+		System.out.println("O troco é " + troco);		
 		
-	    result ="\nTroco = R$"+ formatador.format(troco) +"\n\n";
-	    System.out.println(result);
-		
-
-		// definindo os centavos do troco (parte fracionária)
-		      vlr = (int)Math.round((troco - (int)troco) * 100);
-		      i = 0;
-		      while (vlr != 0) {
-		        ct = vlr / centavos[i]; // calculando a qtde de moedas
-		        if (ct != 0) {
-		           result = result + (ct +"moeda(s) de"+ centavos[i] +
-		           "centavo(s)\n");
-		           vlr = vlr % centavos[i]; // sobra
-		        }
-		        i = i + 1; // próximo centavo
-		      }
-
-		      return(result);
+	// definindo os centavos do troco (parte fracionária)
+		double vlr, ct;
+		int i;
 		
 		
-	}
-
+		double centavos[] = { 0.25, 0.10, 0.5, 0.1 };
+		
+			vlr = (int) Math.round((troco - (int) troco) * 100);
+			i = 0;
+			
+			while (vlr != 0) {
+				ct = vlr / centavos[i]; // calculando a qtde de moedas
+				
+				if (ct != 0) {
+					result = result + (ct + "moeda(s) de" + centavos[i] + "centavo(s)\n");
+					vlr = vlr % centavos[i]; // sobra
+				}
+				i = i + 1; // próximo centavo
+			}
+				
+			}
 	}
 
 
